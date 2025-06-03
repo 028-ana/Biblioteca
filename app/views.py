977e78b1ef2c05c182735ce5213384b19905f79d
028-ana <anaclara.evg@gmail.com>
2025-06-03 09:23:45 -0300

    Add files via upload

--- app/views.py
+++ app/views.py
@@ -1,24 +1,24 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render,redirect,get_object_or_404
 from .models import *
 from django.views import View
+from django.contrib import messages
+# Create your views here.
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
-        livros = Livro.objects.all()
-        return render(request, 'index.html', {'livros': livros})
+        return render (request, 'index.html')
 
 class LivrosView(View):
     def get(self, request, *args, **kwargs):
         livros = Livro.objects.all()
-        return render(request, 'livros.html', {'livros': livros})
-    def post(self, request, *args, **kwargs):
-        pass
-
-# Emprestimo não está funcionando no site
-# class EmprestimoView(View):
-#     def get(self, request, *args, **kwargs):
-#         reservas = Emprestimo.objects.all()
-#         return render(request, 'reserva.html', {'reservas': reservas})
+        return render(request, 'livros.html', {'livros':
+livros})
+# def post(self, request, *args, **kwargs):
+class EmprestimoView(View):
+    def get(self, request, *args, **kwargs):
+        reservas = Emprestimo.objects.all()
+        return render(request, 'reserva.html',
+{'reservas': reservas})
 
 class CidadesView(View):
     def get(self, request, *args, **kwargs):
@@ -45,46 +45,20 @@ class GenerosView(View):
         generos = Genero.objects.all()
         return render(request, 'genero.html', {'generos': generos})
 
+from django.views import View
+from django.shortcuts import redirect, get_object_or_404
+from django.contrib import messages
+from .models import Livro  # ajuste conforme o local do seu model
+
 class DeleteLivroView(View):
     def get(self, request, id, *args, **kwargs):
-        livro = Livro.objects.get(id=id)
-        livro.delete().messages.success(request, 'Livro excluído com sucesso!') # Success message
-        return redirect('livros')
-
-class EditarLivroView(View):
-    template_name = 'editar_livro.html'
-
-    def get(self, request, id, *args, **kwargs):
         livro = get_object_or_404(Livro, id=id)
-        form = LivroForm(instance=livro)
-        return render(request, self.template_name, {
-            'livro': livro,
-            'form': form
-        })
+        livro.delete()
+        messages.success(request, 'Livro excluído com sucesso!')  # Mensagem de sucesso
+        return redirect('livros')  # Redireciona para a página de livros
 
-    def post(self, request, id, *args, **kwargs):
-        livro = get_object_or_404(Livro, id=id)
-        form = LivroForm(request.POST, instance=livro)
-        if form.is_valid():
-            form.save()
-            return redirect('nome_da_url_de_listagem')  # ajuste conforme sua URL
-        return render(request, self.template_name, {
-            'livro': livro,
-            'form': form
-        })
-
-    
-        form = LivroForm(request.POST, instance=livro)
-        if form.is_valid():
-            form.save().messages.success(request, 'As edições foram salvas com sucesso.')
-            return redirect('editar', id=id)  # Redirecionar de volta para a página de edição
-        else:
-            messages.error(request, 'Corrija os erros no formulário antes de enviar novamente.')
-            return render(request, self.template_name, {
-            'livro': livro,
-            'form': form
-        })
-
-
+from django.contrib import admin
+from django.urls import path
+from django.views.generic import TemplateView
 
 
